61a62a6bb9d56d6acce4d0d2269119deb8bf3934
kaige0207 <k1510207@163.com>
2020-12-03 10:01:23 +0800

    120301

--- Week02/main.go
+++ Week02/main.go
@@ -4,7 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/pkg/errors"
+	"log"
 )
+
 /*
 	题目：我们在数据库操作的时候，比如dao 层中当遇到一个sql.ErrNoRows的时候，是否应该 Wrap 这个error，抛给上层。为什么，应该怎么做请写出代码？
 	回答：应该和其它错误一样 Wrap 向上抛给调用者。这样上层业务可以得到堆栈信息，然后利用 errors.Is()方法判断是否是sql.ErrNoRows错误，进行处理
@@ -41,14 +43,14 @@ func main() {
 	user, err := bizFindUserById(1234567)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			fmt.Printf("user not exists: %+v\n", err)
+			log.Printf("user not exists: %+v\n", err)
 			return
 		}
 
-		fmt.Printf("query user failed: %+v\n", err)
+		log.Printf("query user failed: %+v\n", err)
 		return
 	}
 
-	fmt.Printf("query user success: user=%+v\n", user)
+	log.Printf("query user success: user=%+v\n", user)
 
 }
